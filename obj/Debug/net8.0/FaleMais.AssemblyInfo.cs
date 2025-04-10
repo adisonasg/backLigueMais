1685a5155eab71c7b70001d58988697fbdd35628
adisonasg <adison.asg@gmail.com>
2025-04-10 14:23:14 -0300

    second commit

--- obj/Debug/net8.0/FaleMais.AssemblyInfo.cs
+++ obj/Debug/net8.0/FaleMais.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FaleMais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2145c801ab5641e47946d08fe3476331e7e44f86")]
 [assembly: System.Reflection.AssemblyProductAttribute("FaleMais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FaleMais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
